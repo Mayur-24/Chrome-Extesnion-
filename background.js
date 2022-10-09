2f4fc54210256d54e3f5f28e02d731182aa5ad7b
Mayur <agarwalmayur08@gmail.com>
2022-10-10 01:38:06 +0530

    Resolved error of servive_worker

--- background.js
+++ background.js
@@ -7,9 +7,7 @@ chrome.tab.onUpdated.addListener((tabId, tab) => {
     if(tab.url && tab.url.includes("youtube.com/watch")) {  //checking is url exists and if yes is it of youtube
         const queryParameters = tab.url.split("?")[1];
         const urlParameters = new URLSearchParams(queryParameters);
-        /**
-        * there is a messaging system that happens between extension, we send message to contentScript that a new video has been loaded and this is the id of video
-        **/
+        // there is a messaging system that happens between extension, we send message to contentScript that a new video has been loaded and this is the id of video
         console.log(urlParameters);
         chrome.tabs.sendMessage(tabId, {
         type: "NEW",
