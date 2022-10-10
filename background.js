60db56d0dda369541189f8e2677644800cc915d2
Mayur <agarwalmayur08@gmail.com>
2022-10-10 13:59:17 +0530

    Finally getting console.log

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 * to any updates in our tab system and find the most recent tab
 * and check if that is of youtube and if t is of youtube then we will add + button to it
 **/
-chrome.tab.onUpdated.addListener((tabId, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if(tab.url && tab.url.includes("youtube.com/watch")) {  //checking is url exists and if yes is it of youtube
         const queryParameters = tab.url.split("?")[1];
         const urlParameters = new URLSearchParams(queryParameters);
